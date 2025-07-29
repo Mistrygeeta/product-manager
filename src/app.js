fab5a3e4dd789b6cbded5bd1ac2ad00b42faf5cf
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-07-30 00:01:45 +0530

    post api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
-
+const productModel = require("./models/product.model")
 const app = express();
 
+app.use(express.json())
 
+
+app.post("/products", async(req, res)=>{
+    try{
+        const {name, price}= req.body;
+        const product = new productModel({name, price})
+        await product.save();
+        res.status(201).json({
+            message : "product created successfully",
+            product
+        })
+    }catch(err){
+        res.status(500).json({
+        messagec: "error in creatig product",
+        error: err.message
+        })
+    }
+})
 
 module.exports = app;
