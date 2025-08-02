da68b6086d635113012a1a885ffd7d65c7b1c7af
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-08-03 00:03:52 +0530

    add upadte and delete route for product

--- src/app.js
+++ src/app.js
@@ -27,7 +27,8 @@ app.get("/products", async(req, res)=>{
     try{
         const product = await productModel.find()
         res.status(200).json({
-            message: "product fetched successfully"
+            message: "product fetched successfully",
+            product
         })
     }
     catch(err){
@@ -53,6 +54,49 @@ app.get("/products/:id", async(req, res)=>{
     catch (err){
         res.status(500).json({
             message : "error",
+            error: err.message
+        })
+    }
+})
+
+app.delete("/products/:id",async(req, res)=>{
+    try{
+        const product = await productModel.findByIdAndDelete(req.params.id)
+        if(!product){
+            return res.status(404).json({
+                message: "product not found"
+            })
+        }
+       res.status(200).json({
+        message : "product deleted successfully",
+        product
+       })
+
+    }catch(err){
+        res.status(500).json({
+            message:"error deleting product",
+            error: err.message
+        })
+    }
+})
+
+app.patch("/products/:id", async(req, res)=>{
+    try{
+        const id = req.params.id
+        const{price}= req.body
+        const product =await productModel.findByIdAndUpdate( id,{price})
+        if(!product){
+            return res.status(404).json({
+                message: "product not found"
+            })
+        }
+      res.status(200).json({
+        message: "product updated successfully",
+        product
+      })
+    }catch(err){
+        res.status(500).json({
+            message : "error upadting products",
             error: err.message
         })
     }
